beb5b9a205c7ffe7db9ae4cb011cfd56c6a4d682
alex parsons <alex parsons>
2016-12-31 15:58:12 -0800

    asd

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+//For avoidong Heroku $PORT error
+app.get('/', function (request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function () {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
