142c70cca2290599274a037c1c139134e4896db5
alex parsons <alex parsons>
2016-12-31 15:28:46 -0800

    as

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.listen(3000);
