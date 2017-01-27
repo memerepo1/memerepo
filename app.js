7f506c33a00bb5ab81ee023e3d2e51a4e8d1b2a8
alex parsons <alex parsons>
2017-01-27 01:06:01 -0800

    commit

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/views/html'));
 
 app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
-app.get('/', function (request, response) {
+app.get('/', function (req, res) {
     var result = 'App is running'
-    response.send(result);
+    res.send(result);
+    res.render('./views/html/index.html');
 }).listen(app.get('port'), function () {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
+
